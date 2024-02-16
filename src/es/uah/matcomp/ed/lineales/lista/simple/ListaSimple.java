c68ab6457a8290afb08084733438727bd91e5e5c
candelasanzm <sanzmartincandela@gmail.com>
2024-02-16 13:16:26 +0100

    elemento

--- src/es/uah/matcomp/ed/lineales/lista/simple/ListaSimple.java
+++ src/es/uah/matcomp/ed/lineales/lista/simple/ListaSimple.java
@@ -4,4 +4,41 @@ package es.uah.matcomp.ed.lineales.lista.simple;
  * Programar la lista simplemente enlazada.
  */
 public class ListaSimple {
+    private ElementoLS [] data;
+    private int maximo;
+    public void isVacia() {
+        Boolean data = null;
+    }
+    protected void add (ElementoLS el) {
+        data[0] = el;
+    }
+    public void add (String s, int i) {
+    }
+    public void add (Object o) {
+    }
+    public void setElement (String s, int posicion){
+    }
+    public void setElement (Object o, int posicion) {
+    }
+    public void remove (int posicion) {
+        data[0] = null;
+    }
+    public int getNumeroElementos (){
+        return 0;
+    }
+    public int getPosicion(ElementoLS el){
+        return 0;
+    }
+    public ElementoLS getPrimero(){
+        return null;
+    }
+    public ElementoLS getUltimo(){
+        return null;
+    }
+    protected ElementoLS getSiguiente(ElementoLS el){
+        return null;
+    }
+    public ElementoLS getElemento(int posicion){
+        return null;
+    }
 }
--- src/main.java
+++ src/main.java
@@ -7,8 +7,8 @@ public class main {
     public static void main(String[] s){
         ListaSimple l = new ListaSimple();
 
-        l.add("Primer elemento");
-        l.add("Segundo elemento");
+        l.add("Primer elemento", 1);
+        l.add("Segundo elemento", 1);
         ElementoLS elemento = l.getElemento(1);
         System.out.println(elemento.getData());
         System.out.println("Datos detallados");
