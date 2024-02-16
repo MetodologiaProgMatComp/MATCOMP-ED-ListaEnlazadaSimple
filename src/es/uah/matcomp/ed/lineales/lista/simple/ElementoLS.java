b4cd94d32eca7c42033e30cf708761fb843c2f23
candelasanzm <sanzmartincandela@gmail.com>
2024-02-16 12:17:23 +0100

    elemento

--- src/es/uah/matcomp/ed/lineales/lista/simple/ElementoLS.java
+++ src/es/uah/matcomp/ed/lineales/lista/simple/ElementoLS.java
@@ -1,4 +1,13 @@
 package es.uah.matcomp.ed.lineales.lista.simple;
 
 public class ElementoLS {
+    private Object data ;
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
 }
