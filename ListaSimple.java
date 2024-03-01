b68224087d6cb31fb1a04ed8b192873fb49acf69
danielarojast11 <daniela.rojast@edu.uah.es>
2024-03-01 12:55:11 +0100

    setEelement

--- ListaSimple.java
+++ ListaSimple.java
@@ -6,39 +6,79 @@ package es.uah.matcomp.ed.lineales.lista.simple;
 public class ListaSimple {
     private ElementoLS [] data;
     private int maximo;
-    public void isVacia() {
-        Boolean data = null;
+    public boolean isVacia() {
+        if (data.length == 0) {
+            return true;
+        }
+
+        return false;
+    }
+    public  void vaciar() {
+        ElementoLS [] data;
     }
     protected void add (ElementoLS el) {
-        data[0] = el;
+        data[data.length -1] = el;
     }
     public void add (String s, int i) {
+        ElementoLS elementoLS = new ElementoLS();
+        elementoLS.setData(new Object());
+        this.add(elementoLS);
     }
     public void add (Object o) {
+        ElementoLS elementoLS = new ElementoLS();
+        elementoLS.setData(o);
+        this.add(elementoLS);
     }
     public void setElement (String s, int posicion){
+        ElementoLS elementoLS = new ElementoLS();
+        elementoLS.setData(new String());
+        this.setElement(elementoLS, posicion);
     }
     public void setElement (Object o, int posicion) {
+        ElementoLS elementoLS = new ElementoLS();
+        elementoLS.setData(new Object());
+        this.setElement(elementoLS, posicion);
     }
     public void remove (int posicion) {
-        data[0] = null;
+        data[posicion] = null;
     }
-    public int getNumeroElementos (){
-        return 0;
+    public int getNumeroElementos(){
+        int i = 0;
+        int j = 0;
+        while (i < data.length){
+            if (data[i] != null){
+                j ++;
+            }
+            i++;
+        }
+        return j;
     }
     public int getPosicion(ElementoLS el){
-        return 0;
+        int i=0;
+        while (el!=data[i]) {
+            i++;
+        }
+        return i;
     }
     public ElementoLS getPrimero(){
-        return null;
+        ElementoLS j;
+        j = data[0];
+        return j;
     }
     public ElementoLS getUltimo(){
-        return null;
+        int i = data.length;
+        ElementoLS j;
+        j = data[i];
+        return j;
     }
     protected ElementoLS getSiguiente(ElementoLS el){
-        return null;
+        int i = getPosicion(el);
+        i++;
+        ElementoLS j = data[i];
+        return j;
     }
     public ElementoLS getElemento(int posicion){
-        return null;
+        ElementoLS j = data[posicion];
+        return j;
     }
 }
