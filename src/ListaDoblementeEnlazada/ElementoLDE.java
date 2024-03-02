f3f5b73ebbabef67de9c9b9ecfcdb65cef2a8aec
candelasanzm <sanzmartincandela@gmail.com>
2024-03-02 20:40:30 +0100

    elemento

--- src/ListaDoblementeEnlazada/ElementoLDE.java
+++ src/ListaDoblementeEnlazada/ElementoLDE.java
@@ -4,21 +4,22 @@ public class ElementoLDE {
     private ElementoLDE anterior;
     private ElementoLDE siguiente;
     private Object data;
-    protected insertarmeEn (ElementoLDE el){
-        ElementoLDE el = new ElementoLDE();
-        el.setData(new ElementoLDE());
-        this.setData(el);
+    protected void insertarmeEn (ElementoLDE el){
+        el.siguiente = this.siguiente;
+        this.siguiente = el;
+        el.anterior = this;
     }
     protected ElementoLDE getSiguiente(){
-        return siguiente += 1;
+        return this.siguiente;
     }
     protected ElementoLDE getAnterior(){
-
+        return this.anterior;
     }
     public Object getData(){
         return data;
     }
     public Object setData(Object o){
+        this.data = o;
         return data;
     }
 }
--- src/ListaDoblementeEnlazada/ListaDoblementeEnlazada.java
+++ src/ListaDoblementeEnlazada/ListaDoblementeEnlazada.java
@@ -1,26 +1,37 @@
 package ListaDoblementeEnlazada;
 
-import es.uah.matcomp.ed.lineales.lista.simple.ElementoLS;
-
 public class ListaDoblementeEnlazada {
     private ElementoLDE primero;
     private ElementoLDE ultimo;
     public boolean isVacia(){
-        if (data.length == 0){
+        if (primero == null){
             return true;
         }
         return false;
     }
     public void vaciar(){
-        ElementoLDE [] data;
+        primero = null;
     }
     protected int add (ElementoLDE el){
-        data[data.length -1] = el;
-        return data.length;
+        if (primero == null){
+            primero = el;
+        }
+        else{
+            ultimo.insertarmeEn(el);
+        }
+        ultimo = el;
+        ElementoLDE posicion = primero;
+        int contador = 0;
+        while (posicion.getSiguiente() != null){
+            contador ++;
+            posicion = posicion.getSiguiente();
+        }
+        return contador;
     }
     public void add (String s){
         ElementoLDE elementoLDE = new ElementoLDE();
         elementoLDE.setData(s);
+        this.add(elementoLDE);
     }
     public void add (Object o){
         ElementoLDE elementoLDE = new ElementoLDE();
@@ -29,13 +40,25 @@ public class ListaDoblementeEnlazada {
     }
     public void insert (String s, int posicion){
         ElementoLDE elementoLDE = new ElementoLDE();
-        elementoLDE.setData(new String());
-        this.insert(elementoLDE, posicion);
+        elementoLDE.setData(s);
+        int contador = 0;
+        ElementoLDE actual = primero;
+        while (contador < posicion){
+            actual = actual.getSiguiente();
+            contador ++;
+        }
+        actual.insertarmeEn(elementoLDE);
     }
     public void insert(Object o, int posicion){
         ElementoLDE elementoLDE = new ElementoLDE();
-        elementoLDE.setData(new Object());
-        this.insert(elementoLDE, posicion);
+        elementoLDE.setData(o);
+        int contador = 0;
+        ElementoLDE actual = primero;
+        while (contador < posicion){
+            actual = actual.getSiguiente();
+            contador ++;
+        }
+        actual.insertarmeEn(elementoLDE);
     }
     public int del (int posicion){
         data[posicion] = null;
